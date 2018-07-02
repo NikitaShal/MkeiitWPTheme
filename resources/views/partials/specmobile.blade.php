1d45ac66603132c4deac70d0cf7d18edd9cc1b02
nikitashal <shalomyshka@gmail.com>
2018-07-02 11:42:38 +0300

    bugs added

--- resources/views/partials/specmobile.blade.php
+++ resources/views/partials/specmobile.blade.php
@@ -3,31 +3,36 @@
   <div class="specmobile">
 
     <h4 class="specname" style="padding-top: 30px;">
-      Программирование
+      <?php echo get_theme_mod( 'setting_kurs1_name' ); ?>
     </h4>
 
     <ul class="speclist">
-      <li>Программирование в компьютерных системах</li>
-      <li>Компьютерные сети</li>
-      <li>Прикладная информатика</li>
+      <li><?php echo get_theme_mod( 'setting_kurs1_spec1_name' ); ?></li>
+      <li><?php echo get_theme_mod( 'setting_kurs1_spec2_name' ); ?></li>
+      <li><?php echo get_theme_mod( 'setting_kurs1_spec3_name' ); ?></li>
+      <li><a href="<?php echo get_theme_mod( 'f1_course_url_settings' ); ?>"><h4 class="more__button__mobile">Подробнее..</h4></a></li>
+
     </ul>
 
     <h4 class="specname">
-      Экономика и бухгалтерия
+      <?php echo get_theme_mod( 'setting_kurs2_name' ); ?>
     </h4>
 
     <ul class="speclist">
-      <li>Экономика и бухгалтерский учет</li>
-      <li>Банковское дело</li>
+      <li><?php echo get_theme_mod( 'setting_kurs2_spec1_name' ); ?></li>
+      <li><?php echo get_theme_mod( 'setting_kurs2_spec2_name' ); ?></li>
+      <li><?php echo get_theme_mod( 'setting_kurs2_spec3_name' ); ?></li>
+      <li><a href="<?php echo get_theme_mod( 'f2_course_url_settings' ); ?>"><h4 class="more__button__mobile">Подробнее..</h4></a></li>
     </ul>
 
     <h4 class="specname">
-      Товароведение
+      <?php echo get_theme_mod( 'setting_kurs3_name' ); ?>
     </h4>
 
     <ul class="speclist">
-      <li>Технология продукции общественного питания</li>
-      <li>Товароведение</li>
+      <li><?php echo get_theme_mod( 'setting_kurs3_spec1_name' ); ?></li>
+      <li><?php echo get_theme_mod( 'setting_kurs3_spec2_name' ); ?></li>
+      <li><a href="<?php echo get_theme_mod( 'f3_course_url_settings' ); ?>"><h4 class="more__button__mobile">Подробнее..</h4></a></li>
     </ul>
 
     <br>
